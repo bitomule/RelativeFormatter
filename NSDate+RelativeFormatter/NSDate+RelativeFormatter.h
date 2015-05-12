93f25f2e049f06a776cb9114869cb9056e3ed990
David Collado Sela <bitomule@gmail.com>
2015-05-12 14:52:19 +0200

    Initial Commit

---
+++ NSDate+RelativeFormatter/NSDate+RelativeFormatter.h
@@ -0,0 +1,19 @@
+//
+//  NSDate+RelativeFormatter.h
+//  NSDate+RelativeFormatter
+//
+//  Created by David Collado Sela on 12/5/15.
+//  Copyright (c) 2015 David Collado Sela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSDate+RelativeFormatter.
+FOUNDATION_EXPORT double NSDate_RelativeFormatterVersionNumber;
+
+//! Project version string for NSDate+RelativeFormatter.
+FOUNDATION_EXPORT const unsigned char NSDate_RelativeFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDate_RelativeFormatter/PublicHeader.h>
+
+
