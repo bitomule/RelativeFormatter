ba817a4865af7e51080ef9c4384b8c06514ee6b2
David Collado Sela <bitomule@gmail.com>
2015-05-12 15:37:27 +0200

    basic timeago

---
+++ NSDate+RelativeFormatter/NSDate+RelativeFormatter.swift
@@ -0,0 +1,55 @@
+//
+//  NSDate+RelativeFormatter.swift
+//  NSDate+RelativeFormatter
+//
+//  Created by David Collado Sela on 12/5/15.
+//  Copyright (c) 2015 David Collado Sela. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate{
+    
+    enum Format{
+        case Seconds,Hours,Days,Weeks,Months,Years
+    }
+    
+    func relativeFormatted(format:Format=Format.Seconds)->String{
+        let calendar = NSCalendar.currentCalendar()
+        let unitFlags = NSCalendarUnit.CalendarUnitMinute | NSCalendarUnit.CalendarUnitHour | NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitWeekOfYear | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear | NSCalendarUnit.CalendarUnitSecond
+        let now = NSDate()
+        let earliest = now.earlierDate(self)
+        let latest = (earliest == now) ? self : now
+        let components:NSDateComponents = calendar.components(unitFlags, fromDate: earliest, toDate: latest, options: nil)
+        
+        if(components.year >= 2){
+            return "\(components.year) years ago"
+        }
+        if(components.year >= 1){
+            return "1 year ago"
+        }
+        if(components.month >= 2){
+            return "\(components.month) months ago"
+        }
+        if(components.month >= 1){
+            return "1 month ago"
+        }
+        if(components.weekOfYear >= 2){
+            return "\(components.weekOfYear) weeks ago"
+        }
+        if(components.weekOfYear >= 1){
+            return "1 week ago"
+        }
+        if(components.day >= 2){
+            return "\(components.day) days ago"
+        }
+        if(components.day >= 1){
+            return "1 day ago"
+        }
+        if(components.second < 60){
+            return "\(components.second) seconds ago"
+        }
+        return ""
+    }
+    
+}
--- NSDate+RelativeFormatterTests/NSDate_RelativeFormatterTests.swift
+++ NSDate+RelativeFormatterTests/NSDate_RelativeFormatterTests.swift
@@ -21,16 +21,72 @@ class NSDate_RelativeFormatterTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testYearsAgo(){
+        let twoYearsAgo = dateYearsAgo(2)
+        XCTAssertEqual(twoYearsAgo.relativeFormatted(), "2 years ago", "Hace 2 años")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testYearAgo(){
+        let oneYearAgo = dateYearsAgo(1)
+        XCTAssertEqual(oneYearAgo.relativeFormatted(), "1 year ago", "Hace 1 año")
     }
+    
+    func testMonthsAgo(){
+        let twoMonthsAgo = dateMonthsAgo(2)
+        XCTAssertEqual(twoMonthsAgo.relativeFormatted(), "2 months ago", "Hace 2 meses")
+    }
+    
+    func testMonthAgo(){
+        let oneMonthAgo = dateMonthsAgo(1)
+        XCTAssertEqual(oneMonthAgo.relativeFormatted(), "1 month ago", "Hace 1 mes")
+    }
+    
+    func testWeeksAgo(){
+        let twoWeeksAgo = dateWeeksAgo(2)
+        XCTAssertEqual(twoWeeksAgo.relativeFormatted(), "2 weeks ago", "Hace 2 semanas")
+    }
+    
+    func testWeekAgo(){
+        let oneWeekAgo = dateWeeksAgo(1)
+        XCTAssertEqual(oneWeekAgo.relativeFormatted(), "1 week ago", "Hace 1 semana")
+    }
+    
+    func testDaysAgo(){
+        let twoDaysAgo = dateDaysAgo(2)
+        XCTAssertEqual(twoDaysAgo.relativeFormatted(), "2 days ago", "Hace 2 dias")
+    }
+    
+    func testDayAgo(){
+        let oneDayAgo = dateDaysAgo(1)
+        XCTAssertEqual(oneDayAgo.relativeFormatted(), "1 day ago", "Hace 1 dia")
+    }
+    
+    func testSecondsAgo() {
+        let tenSecondsAgo = dateSecondsAgo(10)
+        XCTAssertEqual(tenSecondsAgo.relativeFormatted(), "10 seconds ago", "Hace 10 segundos")
+    }
+    
+    //MARK - Helpers
+    
+    func dateYearsAgo(years:Int) -> NSDate{
+        return NSCalendar.currentCalendar().dateByAddingUnit(NSCalendarUnit.CalendarUnitYear, value: years * -1, toDate: NSDate(), options: nil)!
+    }
+    
+    func dateMonthsAgo(months:Int) -> NSDate{
+        return NSCalendar.currentCalendar().dateByAddingUnit(NSCalendarUnit.CalendarUnitMonth, value: months * -1, toDate: NSDate(), options: nil)!
+    }
+    
+    func dateWeeksAgo(weeks:Int) -> NSDate{
+        return NSCalendar.currentCalendar().dateByAddingUnit(NSCalendarUnit.CalendarUnitWeekOfYear, value: weeks * -1, toDate: NSDate(), options: nil)!
+    }
+    
+    func dateDaysAgo(days:Int) -> NSDate{
+        return NSCalendar.currentCalendar().dateByAddingUnit(NSCalendarUnit.CalendarUnitDay, value: days * -1, toDate: NSDate(), options: nil)!
+    }
+    
+    func dateSecondsAgo(seconds:Int) -> NSDate{
+        return NSCalendar.currentCalendar().dateByAddingUnit(NSCalendarUnit.CalendarUnitSecond, value: seconds * -1, toDate: NSDate(), options: nil)!
+    }
+
     
 }
